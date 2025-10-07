32fe6d411fcc31b471596dfe503fcfbcef0a69c0
Németh Balázs <nemethbalazs2007@gmail.com>
2025-10-07 12:28:56 +0200

    valami

--- fajlteszt.py
+++ fajlteszt.py
@@ -1,13 +1,45 @@
+verseny_adatok = []
+
 try:
     with open("F1-2024dec.csv" ,encoding="utf-8") as fajl:
-        n = 4 / 0
-        f = fajl.read()
-    print(f)
-    print(n)
+        verseny_adatok = fajl.readlines()
+        
+except Exception as ex:
+    print(f"Halihóóóó! : Hiba oka: {ex}")
 except FileNotFoundError:
     print("Hiba a fájl megnyitása közben!")
-except ZeroDivisionError:
-    print("Ne ossz 0-val!")
-except Exception:
-    print("Halihóóóó!")
-print("Itt a program vége!")
+
+print(verseny_adatok)
+
+
+""""
+    1. [] Megszámolás
+    2. [] Eldöntés 1
+       [] Eldöntés 2
+    3. [] Kiválasztás
+    4. [] Keresés
+    5. [] Sorozatszámítás, összegzés 
+    6. [] Minimum/maximum kiválasztás
+    7. [] Másolás
+    8. [] Kiválogatás
+    9. [] Szétválogatás
+    10 [] Metszet
+    11.[] Unió 
+    12. Rendezés
+        [] Egyszerű cserés rendezés
+        [] Buborékos
+        [] Minimum kiválasztásos
+"""
+
+#----------------------------------------------------------
+# 1. Hány versenyző nem szerzett még pontot?
+db = 0
+
+for i in range(1, len(verseny_adatok)):
+    if(int(verseny_adatok[i].split(',')[1]) == 0):
+        db = db + 1
+
+print(f"{db} versenyző nem szerzett mép pontot.\n")
+
+#----------------------------------------------------------
+#2. 
